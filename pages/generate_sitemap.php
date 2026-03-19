0f1995fa0621b116a22713405bd2f9525f9a1ace
Vikashmahto45 <vikashgpt4545@gmail.com>
2026-03-19 18:37:00 +0530

    FIX: Rebuilt sitemap generator with dynamic scanning and fixed constants

--- pages/generate_sitemap.php
+++ pages/generate_sitemap.php
@@ -1,55 +1,45 @@
-<?php 
-include '../includes/header.php'; 
-$page_title = ""; 
-$meta_description = ""; 
-$meta_keywords = ""; 
-?>
+<?php
+/**
+ * Univindia Sitemap Generator - Fresh Rebuild
+ * This script scans the /pages directory and generates sitemap.xml in the root.
+ */
 
-<?xml version="1.0" encoding="UTF-8"?>' . PHP_EOL;
+require_once __DIR__ . '/../includes/config.php';
+
+$pages_dir = __DIR__ . '/';
+$root_dir = __DIR__ . '/../';
+$xml_file = $root_dir . 'sitemap.xml';
+
+$xml = '<?xml version="1.0" encoding="UTF-8"?>' . PHP_EOL;
 $xml .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' . PHP_EOL;
 
-// Add Home Page
+// 1. Add Home Page
 $xml .= '  <url>' . PHP_EOL;
-$xml .= '    <loc>' . BASE_URL . 'index.php</loc>' . PHP_EOL;
+$xml .= '    <loc>' . BASE_URL . '</loc>' . PHP_EOL;
 $xml .= '    <changefreq>daily</changefreq>' . PHP_EOL;
 $xml .= '    <priority>1.0</priority>' . PHP_EOL;
 $xml .= '  </url>' . PHP_EOL;
 
-// Add Results Page
-$xml .= '  <url>' . PHP_EOL;
-$xml .= '    <loc>' . BASE_URL . 'results.php</loc>' . PHP_EOL;
-$xml .= '    <changefreq>daily</changefreq>' . PHP_EOL;
-$xml .= '    <priority>0.9</priority>' . PHP_EOL;
-$xml .= '  </url>' . PHP_EOL;
-
-// Add AdSense Pages
-$compliance_pages = ['about-us.php', 'privacy-policy.php', 'contact-us.php', 'disclaimer.php', 'terms-and-conditions.php'];
-foreach ($compliance_pages as $cp) {
-    $xml .= '  <url>' . PHP_EOL;
-    $xml .= '    <loc>' . BASE_URL . $cp . '</loc>' . PHP_EOL;
-    $xml .= '    <changefreq>monthly</changefreq>' . PHP_EOL;
-    $xml .= '    <priority>0.5</priority>' . PHP_EOL;
-    $xml .= '  </url>' . PHP_EOL;
-}
-
-// Add Dynamic Pages from link_map
-if (isset($link_map) && is_array($link_map)) {
-    foreach ($link_map as $keyword => $url) {
-        $xml .= '  <url>' . PHP_EOL;
-        $xml .= '    <loc>' . BASE_URL . $url . '</loc>' . PHP_EOL;
-        $xml .= '    <changefreq>weekly</changefreq>' . PHP_EOL;
-        $xml .= '    <priority>0.8</priority>' . PHP_EOL;
-        $xml .= '  </url>' . PHP_EOL;
+// 2. Scan and Add All SEO Pages
+if (is_dir($pages_dir)) {
+    $files = scandir($pages_dir);
+    foreach ($files as $file) {
+        if ($file !== '.' && $file !== '..' && substr($file, -4) === '.php' && $file !== 'generate_sitemap.php') {
+            $xml .= '  <url>' . PHP_EOL;
+            $xml .= '    <loc>' . BASE_URL . $file . '</loc>' . PHP_EOL;
+            $xml .= '    <changefreq>weekly</changefreq>' . PHP_EOL;
+            $xml .= '    <priority>0.8</priority>' . PHP_EOL;
+            $xml .= '  </url>' . PHP_EOL;
+        }
     }
 }
 
 $xml .= '</urlset>';
 
-// Write to file
-if (file_put_contents('sitemap.xml', $xml)) {
-    echo "Sitemap generated successfully! Total entries: " . (count($link_map) + 7);
+// 3. Save to Root
+if (file_put_contents($xml_file, $xml)) {
+    echo "SUCCESS: sitemap.xml generated in the root directory.";
 } else {
-    echo "Failed to generate sitemap.";
+    echo "ERROR: Could not write to " . $xml_file;
 }
-
-<?php include '../includes/footer.php'; ?>
+?>
