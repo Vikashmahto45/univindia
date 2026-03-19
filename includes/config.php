0adad2f2aa0043d5da692d06d14744ad6d00773e
Vikashmahto45 <vikashgpt4545@gmail.com>
2026-03-19 18:37:27 +0530

    FIX: CLI compatibility for config.php and final sitemap updates

--- includes/config.php
+++ includes/config.php
@@ -3,8 +3,8 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 // Automatic Base URL
-$host = $_SERVER['HTTP_HOST'];
-$is_local = (strpos($host, 'localhost') !== false || strpos($host, '127.0.0.1') !== false);
+$host = isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : 'localhost';
+$is_local = (strpos($host, 'localhost') !== false || strpos($host, '127.0.0.1') !== false || !isset($_SERVER['HTTP_HOST']));
 define('BASE_URL', $is_local ? 'http://localhost/univindia/' : 'https://univindia.online/');
 define('ASSETS_URL', BASE_URL . 'css/style.css');
 ?>
